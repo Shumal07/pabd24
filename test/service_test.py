306f373adb51dc561c8b80b7292c1a9928faef98
Shumal07 <shumalp2015@gmail.com>
2024-06-13 20:43:10 +0300

    update service_test

--- test/service_test.py
+++ test/service_test.py
@@ -3,31 +3,16 @@ import numpy as np
 import requests
 from dotenv import dotenv_values
 import pandas as pd
-from joblib import load
 
 endpoint = 'http://127.0.0.1:5000/predict'
 
 config = dotenv_values(".env")
 HEADERS = {"Authorization": f"Bearer {config['APP_TOKEN']}"}
-proxies = {
-  "http": None,
-  "https": None,
+proxies ={
+    "http":None,
+    "https":None,
+
 }
-
-# Load the LabelEncoder
-label_encoder = load('models/le_underground.joblib')
-
-def encode_underground(value):
-    try:
-        return int(label_encoder.transform([value])[0])
-    except ValueError:
-        return int(label_encoder.transform(['unknown'])[0])
-
-def preprocess_record(record):
-    record['underground'] = encode_underground(record['underground'])
-    # Convert all values to standard Python types
-    record = {k: int(v) if isinstance(v, (np.integer, np.int32, np.int64)) else float(v) if isinstance(v, (np.float32, np.float64)) else v for k, v in record.items()}
-    return record
 
 def do_request(data: dict) -> tuple:
     t0 = time.time()
@@ -40,6 +25,7 @@ def do_request(data: dict) -> tuple:
     t = time.time() - t0
     return t, resp['price']
 
+
 def test_100():
     df = pd.read_csv('data/proc/val.csv')
     prices = df['price']
@@ -47,7 +33,6 @@ def test_100():
     records = df.to_dict('records')
     delays, pred_prices = [], []
     for row in records:
-        row = preprocess_record(row)
         t, price = do_request(row)
         delays.append(t)
         pred_prices.append(price)
@@ -55,6 +40,7 @@ def test_100():
     error = np.array(pred_prices) - prices.to_numpy()
     avg_error = np.mean(error)
     print(f'Avg delay: {avg_delay*1000} ms, avg error: {avg_error} RUB')
+
 
 if __name__ == '__main__':
     test_100()
