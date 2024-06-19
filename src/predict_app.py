090eaa1c59ff9e6986fa2ddc62a2bf1eefd74a1e
Shumal07 <shumalp2015@gmail.com>
2024-06-19 23:31:22 +0300

    update predict_app

--- src/predict_app.py
+++ src/predict_app.py
@@ -1,81 +1,64 @@
+"""House price prediction service"""
+from dotenv import dotenv_values
 from flask import Flask, request
-from flask_httpauth import HTTPTokenAuth
 from flask_cors import CORS
 from joblib import load
-from dotenv import dotenv_values
+from flask_httpauth import HTTPTokenAuth
+
+#5000 - flask
+#8000 - gunicorn
 
 app = Flask(__name__)
 CORS(app)
-
-MODEL_PATH = 'models/random_forest_v2.joblib'
-ENCODER_PATHS = {
-    'district': 'models/le_district.joblib',
-    'street': 'models/le_street.joblib',
-    'underground': 'models/le_underground.joblib',
-    'residential_complex': 'models/le_residential_complex.joblib'
-}
 
 config = dotenv_values(".env")
 auth = HTTPTokenAuth(scheme='Bearer')
 
 tokens = {
-    config["APP_TOKEN"]: "user1",
+    config['APP_TOKEN']: "user1",
 }
+model = load('models/catboost.joblib')
+
 
 @auth.verify_token
 def verify_token(token):
     if token in tokens:
         return tokens[token]
 
-label_encoders = {key: load(path) for key, path in ENCODER_PATHS.items()}
-
-def encode_feature(encoder, value):
-    try:
-        return encoder.transform([value])[0]
-    except ValueError:
-        return encoder.transform(['unknown'])[0]
-
 def predict(in_data: dict) -> int:
-    total_meters = float(in_data['total_meters'])
-    floor = int(in_data['floor'])
-    floors_count = int(in_data['floors_count'])
+    """ Predict house price from input data parameters.
+    :param in_data: house parameters.
+    :raise Error: If something goes wrong.
+    :return: House price, RUB.
+    :rtype: int
+    """
     rooms_count = int(in_data['rooms_count'])
-    district = encode_feature(label_encoders['district'], in_data['district'])
-    street = encode_feature(label_encoders['street'], in_data['street'])
-    underground = encode_feature(label_encoders['underground'], in_data['underground'])
-    residential_complex = encode_feature(label_encoders['residential_complex'], in_data['residential_complex'])
-    first_floor = floor == 1
-    last_floor = floor == floors_count
-    price_per_sqm = float(in_data['price_per_sqm'])
+    author_type = str(in_data['author_type'])
+    floor = int(in_data['floor'])
+    street = str(in_data['street'])
+    underground = str(in_data['underground'])
+    floors_count = int(in_data['floors_count'])
+    district = str(in_data['district'])
+    total_meters = float(in_data['total_meters'])
 
-    input_features = [
-        floor,
-        floors_count,
-        rooms_count,
-        total_meters,
-        first_floor,
-        last_floor,
-        price_per_sqm,
-        district,
-        street,
-        underground,
-        residential_complex
-    ]
+    price = model.predict([[rooms_count, author_type, floor, street, underground, floors_count,district, total_meters]])[0]
 
-    model = load(MODEL_PATH)
-    res = model.predict([input_features])
-    return int(res)
+    return int(price.squeeze())
+
 
 @app.route("/")
 def home():
     return '<h1>Housing price service.</h1> Use /predict endpoint'
 
+
 @app.route("/predict", methods=['POST'])
 @auth.login_required
 def predict_web_serve():
+    """Dummy service"""
     in_data = request.get_json()
     price = predict(in_data)
     return {'price': price}
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
